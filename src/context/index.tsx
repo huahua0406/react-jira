65d470f3fd49e54110ece8808bc278d61ae6c1ca
huahua0406 <623072902@qq.com>
2021-03-17 22:24:10 +0800

    fix: warning

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,6 +1,11 @@
 import React, { ReactNode } from 'react';
 import { AuthProvider } from './app-context';
+import { QueryClientProvider, QueryClient } from 'react-query';
 
 export const AppProvider: React.FC<ReactNode> = ({ children }) => {
-    return <AuthProvider>{children}</AuthProvider>;
+    return (
+        <QueryClientProvider client={new QueryClient()}>
+            <AuthProvider>{children}</AuthProvider>;
+        </QueryClientProvider>
+    );
 };
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,17 +1,17 @@
 // 0
 export const isFalsy = (value: unknown) => {
-    return value === 0 ? false: !value
-}
+    return value === 0 ? false : !value;
+};
+
+export const isVoid = (value: unknown) => value === null || value === undefined || value === '';
 
 // 去处对象里面空值
-export  function clearObject(obj: object){
-    const result = {...obj}
-    Object.keys(obj).forEach(key => {
-        // @ts-ignore
-        if(isFalsy(obj[key])){
-            // @ts-ignore
-            delete result[key]
+export function clearObject(obj: { [key: string]: unknown }) {
+    const result = { ...obj };
+    Object.keys(obj).forEach((key) => {
+        if (isVoid(obj[key])) {
+            delete result[key];
         }
-    })
-    return result
+    });
+    return result;
 }
